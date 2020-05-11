374a5b20e6b956004138d655a1dde70074dcd325
Raúl Angel Jiménez <Raulangel51@users.noreply.github.com>
2020-05-11 13:40:42 -0600

    2
    Movimiento de la esfera

---
+++ JimenezRaul_Laboratorio7/Assets/Scripts/MovEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovEnemy : MonoBehaviour
+{
+    //variables
+    public GameObject wp;
+    public List<GameObject> wpList;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        foreach(Transform child in wp.transform)
+        {
+            wpList.Add(child.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent)
+        {
+            agent.SetDestination(wpList[Random.Range(0, wpList.Count - 1)].transform.position);
+        }
+    }
+}
---
+++ JimenezRaul_Laboratorio7/Assets/Scripts/MovHero.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovHero : MonoBehaviour
+{
+    //variables
+    public GameObject wp;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0)) 
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(myRay, out hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
